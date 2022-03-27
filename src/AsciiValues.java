043cd139c176efec4fa9fd2c3fdfe9f32447e23b
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-03-27 23:34:43 +0530

    Create AsciiValues.java

---
+++ src/AsciiValues.java
@@ -0,0 +1,19 @@
+imoport java.util.Scanner;
+
+// ASCII Value of Character
+public class AsciiValue {
+ 
+    public static void main(String[] args)
+    {
+      public static void main(String[] args)
+      {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.nextChar();
+  //      char c = 'e';
+        int ascii = c;
+        System.out.println("The ASCII value of " + c + " is: " + ascii);
+      }
+ 
+        
+    }
+}
